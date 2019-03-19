7b559d56c7703fa3d1c9f6a0b48f742456861354
Oscar Barajas <oscar@dragons.mx>
2019-03-19 10:27:26 -0500

    test

--- src/__test__/components/About.test.js
+++ src/__test__/components/About.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import About from '../../components/About';
+import About from '../components/About';
 
 Enzyme.configure({ adapter: new Adapter() });
-
 describe("Pruebas Unitarias con Jest", () => {
   test("Render de un componente", () => {
     const wrapper = shallow(<About />);
